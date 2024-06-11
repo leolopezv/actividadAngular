00cc2ea698bf0d42bd48588ba137aaf5b69f7914
leolopezv <leonardo.lopez@uees.edu.ec>
2024-06-11 08:21:49 -0500

    Trabajo en clase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './shared/inicio/inicio.component';
+import { NosotrosComponent } from './shared/nosotros/nosotros.component'
+import { ServiciosComponent } from './shared/servicios/servicios.component';
+import { ContactoComponent } from './shared/contacto/contacto.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: '/quienes-somos', pathMatch: 'full' }, // Ruta por defecto
+  { path: '**', redirectTo: '/quienes-somos' } // Ruta comodín para página 404
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
